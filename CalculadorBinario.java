f7fda0cb4cc29581ae825d8ee6a698b1d2e57035
manu <144843962+ManusolJ@users.noreply.github.com>
2023-12-15 08:33:36 +0100

    Add files via upload
    Código comentado.

--- CalculadorBinario.java
+++ CalculadorBinario.java
@@ -14,15 +14,15 @@ public class CalculadorBinario {
                 ip = sc.next();
                 i = 0;
             }
-        }
+        }//Se valida la IP introducida.
 
-        String [] ipElementos = ip.split("\\.");
+        String [] ipElementos = ip.split("\\.");    //Se divide los numeros en el punto,introduciendolos en un array con 4 elementos.
         String resultado = "";
-        for(String octavo : ipElementos){
-            int oct = Integer.parseInt(octavo);
-            String binario = Integer.toBinaryString(oct) + ".";
-            while(binario.length() != 9){
-                binario = 0 + binario;
+        for(String octavo : ipElementos){   //Se asigna cada elemento del array a un String,cada vez que se itera el bucle.
+            int oct = Integer.parseInt(octavo); //Se transforma dicho String en un int.
+            String binario = Integer.toBinaryString(oct) + "."; //Este int se transforma en binario mediante un funcion.
+            while(binario.length() != 9){   //Como los zeros a la izquierda se ignoran,se añade esta parte para que hayan exactamente 8 digitos en el octeto.
+                binario = 0 + binario;  //Se crea un String con todos los octetos.
             }
             resultado += binario;
         }
