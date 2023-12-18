bec164731385e4b4b6166f2834bebbb803a76625
manu <144843962+ManusolJ@users.noreply.github.com>
2023-12-18 11:43:34 +0100

    Add files via upload

--- CalculadorBinario.java
+++ CalculadorBinario.java
@@ -6,7 +6,11 @@ public class CalculadorBinario {
 
         System.out.println("Introduce la IP");
         String ip = sc.next();
+        System.out.println("Introduce la mascara");
+        int mascara = sc.nextInt();
+
         char c;
+        char a;
         for(int i = 0;i < ip.length();i++){
             c = ip.charAt(i);
             if(c < 46 || c > 57 || c == 47 || !ip.contains(".") || ip.length() > 16){
@@ -26,6 +30,50 @@ public class CalculadorBinario {
             }
             resultado += binario;
         }
-        System.out.print(resultado + "\b");
+        System.out.print(resultado + "\b");//TEMPORAL ELIMINAR ANTES DE PRESENTAR
+        System.out.println();//TEMPORAL ELIMINAR ANTES DE PRESENTAR
+
+        resultado = resultado.replace(".","");//Quita los puntos del String.
+        System.out.print(resultado);//TEMPORAL ELIMINAR ANTES DE PRESENTAR
+        System.out.println();//TEMPORAL ELIMINAR ANTES DE PRESENTAR
+        String broadcast = "";
+        String masc;
+
+        for(int i = 0;i < resultado.length();i++){
+            c = resultado.charAt(i);
+            a = masc.charAt(i);
+            if(c == 48 && a == 48){
+                broadcast = broadcast + "0";
+            }
+            else if(c == 49 && a == 49){
+                broadcast = broadcast + "1";
+            }
+            else{
+                broadcast = broadcast + "0";
+            }
+        }
+        resultado = insertarPunto(resultado);
+        System.out.println(resultado);
+    }
+    public static int convertidorBiToDec(String binario){
+        int decimal = Integer.parseInt(binario,2);
+        return decimal;
+    }
+
+    public static String insertarPunto(String bag){
+    String []  nuevoOctavo = new String[bag.length()/8];
+    int s = 0;
+    int e = 8;
+    for(int i = 0 ; i < 4;i++){
+        nuevoOctavo[i] = bag.substring(s,e);
+        s = s + 8;
+        e = e + 8;
+    }
+    String resultado = "";
+    for(int ii = 0; ii < 4;ii++){
+        resultado += nuevoOctavo[ii] + ".";
+    }
+    resultado = resultado + "\b";
+    return resultado;
     }
 }
---
+++ Direccion.java
@@ -0,0 +1,7 @@
+public class Direccion {
+
+    public static void main(String [] args){
+        int decimal = Integer.parseInt("101101101010111",2);
+        System.out.print(decimal);
+    }
+}
